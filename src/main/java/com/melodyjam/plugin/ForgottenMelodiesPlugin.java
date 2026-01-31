6a820f696e0a758a5e4c2b51657d0839dbdc8ba3
Nif <Nif-kun@users.noreply.github.com>
2026-01-31 09:59:16 +0800

    initial

---
+++ src/main/java/com/melodyjam/plugin/ForgottenMelodiesPlugin.java
@@ -0,0 +1,18 @@
+package com.melodyjam.plugin;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
+
+public class ForgottenMelodiesPlugin extends JavaPlugin {
+
+
+    public ForgottenMelodiesPlugin(@NonNullDecl JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+    }
+}
