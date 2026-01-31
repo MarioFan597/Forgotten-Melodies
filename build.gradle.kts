5185919b40567c904922ac23483cd10624851791
Stephen Baynham <sdbaynham@gmail.com>
2026-01-30 21:13:28 -0800

    ignore internal folders, some gradle improvements

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,42 @@
+import org.jetbrains.gradle.ext.Application
+import org.jetbrains.gradle.ext.runConfigurations
+import org.jetbrains.gradle.ext.settings
+
 plugins {
     id("java")
+    id("org.jetbrains.gradle.plugin.idea-ext") version "1.3"
+}
+
+// paths
+val userHome = System.getProperty("user.home")
+var hytaleHome = userHome
+val os = org.gradle.internal.os.OperatingSystem.current()
+if (os.isWindows) {
+    hytaleHome = "$userHome/AppData/Roaming/Hytale"
+} else if (os.isMacOsX) {
+    hytaleHome = "$userHome/Library/Application Support/Hytale"
+} else if (os.isLinux) {
+    hytaleHome = "$userHome/.var/app/com.hypixel.HytaleLauncher/data/Hytale"
+    if (!file(hytaleHome).exists()) {
+        hytaleHome = "$userHome/.local/share/Hytale"
+    }
+}
+val gameHome = "$hytaleHome/install/release/package/game/latest"
+val assetPack = "$gameHome/Assets.zip"
+var serverDir = "${layout.buildDirectory.get().asFile.path}/server"
+
+configurations.all {
+    resolutionStrategy {
+        failOnVersionConflict()
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+    withSourcesJar()
+    withJavadocJar()
 }
 
 group = "com.melodyjam"
@@ -10,7 +47,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly(files("libs/HytaleServer.jar"))
+    implementation(files("libs/HytaleServer.jar"))
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -23,4 +60,13 @@ tasks.test {
 tasks.jar {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
     from("src/main/resources")
+}
+
+idea.project.settings.runConfigurations {
+    create("HytaleServer", Application::class) {
+        mainClass = "com.hypixel.hytale.Main"
+        workingDirectory=serverDir
+        moduleName=project.idea.module.name + ".main"
+        programParameters="--allow-op --disable-sentry --assets=\"$assetPack\""
+    }
 }
