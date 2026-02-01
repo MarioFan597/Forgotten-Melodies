d4d531b9fa5ba6f989a1783ca2118296d3ad806f
Nif <Nif-kun@users.noreply.github.com>
2026-02-02 05:45:06 +0800

    - Added new Prefabs for Earth Path - Added Generic Prefabs to be used for other Paths - Added a breakable block whitelist for the Resonant Ruin. - Added enemy entities for each dungeon room.

--- src/main/java/com/melodyjam/plugin/Systems/BreakRuleSystem.java
+++ src/main/java/com/melodyjam/plugin/Systems/BreakRuleSystem.java
@@ -6,6 +6,8 @@ import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.EntityEventSystem;
+import com.hypixel.hytale.server.core.asset.type.blocktype.config.BlockType;
+import com.hypixel.hytale.server.core.asset.type.item.config.Item;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.event.events.ecs.BreakBlockEvent;
 import com.hypixel.hytale.server.core.universe.world.World;
@@ -13,9 +15,22 @@ import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class BreakRuleSystem extends EntityEventSystem<EntityStore, BreakBlockEvent> {
 
     final String worldName = "Portals_Resonant_Ruin";
+    final String[] whitelist = new String[] {
+            "Deco_SpiderWeb_Full",
+            "Deco_SpiderWeb_Flat",
+            "Deco_SpiderWeb",
+            "Deco_Spider_Cocoon",
+            "Furniture_Temple_Light_Pot",
+            "Rubble_Marble",
+            "Rubble_Marble_Medium",
+            "Wood_Sticks"
+    };
 
     public BreakRuleSystem() {
         super(BreakBlockEvent.class);
@@ -34,6 +49,14 @@ public class BreakRuleSystem extends EntityEventSystem<EntityStore, BreakBlockEv
         World world = player.getWorld();
         assert world != null;
         if (world.getName().contains(worldName)) {
+            BlockType blockType = event.getBlockType();
+            Item item = blockType.getItem();
+            if (item != null) {
+                String itemId = item.getId();
+                if (Arrays.stream(whitelist).anyMatch(itemId::contains)) {
+                    return;
+                }
+            }
             event.setCancelled(true);
         }
     }
