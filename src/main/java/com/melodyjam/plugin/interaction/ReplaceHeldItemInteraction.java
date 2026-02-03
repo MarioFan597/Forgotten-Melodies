04e19be195d0cdc3056e9d072de4cd5701a18d64
Stephen Baynham <sdbaynham@gmail.com>
2026-02-02 18:16:34 -0800

    fix swapping kantele while pointed at sky

--- src/main/java/com/melodyjam/plugin/interaction/ReplaceHeldItemInteraction.java
+++ src/main/java/com/melodyjam/plugin/interaction/ReplaceHeldItemInteraction.java
@@ -16,6 +16,7 @@ import com.hypixel.hytale.server.core.entity.LivingEntity;
 import com.hypixel.hytale.server.core.inventory.Inventory;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.CooldownHandler;
+import com.hypixel.hytale.server.core.modules.interaction.interaction.config.SimpleInstantInteraction;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.config.client.SimpleBlockInteraction;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.util.InteractionTarget;
 import com.hypixel.hytale.server.core.universe.world.World;
@@ -26,10 +27,10 @@ import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
 import javax.annotation.Nonnull;
 
-public class ReplaceHeldItemInteraction extends SimpleBlockInteraction {
+public class ReplaceHeldItemInteraction extends SimpleInstantInteraction {
     @Nonnull
     public static final BuilderCodec<ReplaceHeldItemInteraction> CODEC = BuilderCodec.builder(
-        ReplaceHeldItemInteraction.class, ReplaceHeldItemInteraction::new, SimpleBlockInteraction.CODEC
+        ReplaceHeldItemInteraction.class, ReplaceHeldItemInteraction::new, SimpleInstantInteraction.CODEC
     )
     .documentation("Replacing an item stack held in the target's hand with an identical one with a different item id. Will fail if the player's hand is empty.")
     .append(new KeyedCodec<>("ItemId", Codec.STRING), (i, s) -> i.itemId = s, i -> i.itemId)
@@ -50,7 +51,7 @@ public class ReplaceHeldItemInteraction extends SimpleBlockInteraction {
     private InteractionTarget entityTarget = InteractionTarget.USER;
 
     @Override
-    protected void interactWithBlock(@NonNullDecl World world, @NonNullDecl CommandBuffer<EntityStore> commandBuffer, @NonNullDecl InteractionType interactionType, @NonNullDecl InteractionContext context, @NullableDecl ItemStack itemStack, @NonNullDecl Vector3i vector3i, @NonNullDecl CooldownHandler cooldownHandler) {
+    protected void firstRun(@NonNullDecl InteractionType interactionType, @NonNullDecl InteractionContext context, @NonNullDecl CooldownHandler cooldownHandler) {
         Ref<EntityStore> ref = context.getEntity();
         Ref<EntityStore> targetRef = this.entityTarget.getEntity(context, ref);
 
@@ -73,29 +74,6 @@ public class ReplaceHeldItemInteraction extends SimpleBlockInteraction {
 
         ItemStack replacement = new ItemStack(itemId, stack.getQuantity(), stack.getDurability(), stack.getMaxDurability(), stack.getMetadata());
         inventory.getHotbar().setItemStackForSlot(inventory.getActiveHotbarSlot(), replacement);
-    }
 
-    @Override
-    protected void simulateInteractWithBlock(@NonNullDecl InteractionType interactionType, @NonNullDecl InteractionContext context, @NullableDecl ItemStack itemStack, @NonNullDecl World world, @NonNullDecl Vector3i vector3i) {
-        ItemStack stack = getHeldItem(context);
-        if (stack == null || itemId == null || itemId.isEmpty()) {
-            context.getState().state = InteractionState.Failed;
-        }
-    }
-
-    private ItemStack getHeldItem(@NonNullDecl InteractionContext context) {
-        Ref<EntityStore> ref = context.getEntity();
-        Ref<EntityStore> targetRef = this.entityTarget.getEntity(context, ref);
-
-        if (!(EntityUtils.getEntity(targetRef, context.getCommandBuffer()) instanceof LivingEntity livingEntity)) {
-            return null;
-        }
-
-        Inventory inventory = livingEntity.getInventory();
-        if (inventory == null) {
-            return null;
-        }
-
-        return inventory.getActiveHotbarItem();
     }
 }
